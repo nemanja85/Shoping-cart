8200d383aa50e8c33939ca99d50406e164a93520
nemanja85 <mitic@deutsche-teilkauf.de>
2022-07-01 00:53:30 +0200

    Created context for shopping card

---
+++ vite-project/src/context/ShoppingCartContext.tsx
@@ -0,0 +1,32 @@
+import { createContext, ReactNode, useContext, useState } from 'react';
+
+type ShoppingCartContextProps = {
+  getItems: (id: number) => number;
+  increaseCart: (id: number) => void;
+  decreaseCart: (id: number) => void;
+  removeFromCart: (id: number) => void;
+};
+
+type ShoppingCartProviderProps = {
+  children: ReactNode;
+};
+
+type CartItem = {
+  id: number;
+  quantity: number;
+};
+
+const shoppingCartContext = createContext({} as ShoppingCartContextProps);
+
+export function useShoppingCart() {
+  return useContext(shoppingCartContext);
+}
+
+export function shoppingCartProvider({ children }: ShoppingCartProviderProps) {
+  const [cartItems, setCartItems] = useState<CartItem>();
+  return ( 
+    <shoppingCartContext.Provider value={{}}>
+      {children}
+    </shoppingCartContext.Provider>
+  )
+}
